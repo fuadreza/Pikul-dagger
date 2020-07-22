ef9b941ddad5f507ca7421d7d07701de9f9b5c0a
fuadreza <fuadreza1011@gmail.com>
2020-07-22 22:45:15 +0700

    fix back button home

--- app/src/main/java/io/github/fuadreza/pikul_dagger/ui/main/HomeActivity.kt
+++ app/src/main/java/io/github/fuadreza/pikul_dagger/ui/main/HomeActivity.kt
@@ -2,7 +2,9 @@ package io.github.fuadreza.pikul_dagger.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -85,7 +87,19 @@ class HomeActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.apply {
             replace(R.id.frameContainer, fragment)
-            addToBackStack(null)
         }.commit()
+    }
+
+    private var doubleBackToExitPressedOnce = false
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
     }
 }
