46bf781800b3b7cdaa2892b25458ca9afd5dc4bc
fuadreza <fuadreza1011@gmail.com>
2021-03-01 00:57:28 +0700

    Add fetch user profile, add toast utils

--- app/src/main/java/io/github/fuadreza/pikul_dagger/views/main/profile/ProfileFragment.kt
+++ app/src/main/java/io/github/fuadreza/pikul_dagger/views/main/profile/ProfileFragment.kt
@@ -6,10 +6,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.observe
 import com.google.firebase.auth.FirebaseUser
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.fuadreza.pikul_dagger.R
+import io.github.fuadreza.pikul_dagger.model.UserProfile
+import io.github.fuadreza.pikul_dagger.utils.toast
 import io.github.fuadreza.pikul_dagger.views.setting.SettingActivity
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -18,13 +24,10 @@ import kotlinx.android.synthetic.main.fragment_profile.*
  *
  */
 
-class ProfileFragment : Fragment() {
+@AndroidEntryPoint
+class ProfileFragment : Fragment(), LifecycleOwner {
 
     private val viewModel: ProfileViewModel by viewModels()
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,25 +36,42 @@ class ProfileFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
+        lifecycle.addObserver(viewModel)
+
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        val userManager = (activity?.application as PikulApp).appComponent.userManager()
-
-        //Log.d("PAKET" , "USER " + userManager.user?.displayName)
-
-//        setupViews(view, userManager.user)
+        observe()
     }
 
-    private fun setupViews(view: View, user: FirebaseUser?) {
-        tv_nama.text = user?.displayName
-        tv_email.text = user?.email
-        btn_edit.setOnClickListener {
-            startActivity(Intent(view.context, SettingActivity::class.java))
+    private fun observe() {
+        viewModel.profileState.observe(viewLifecycleOwner){
+            when(it){
+                is ProfileState.ProfileLoaded -> {
+                    setupViews(it.data)
+                }
+                is ProfileState.LoadProfileError -> {
+                    toast("Data gagal diunduh")
+                }
+                is ProfileState.LoadingState -> {
+
+                }
+                else -> {
+
+                }
+            }
         }
+    }
+
+    private fun setupViews(user: UserProfile?) {
+        tv_nama.text = "${user?.firstName} ${user?.lastName}"
+        tv_email.text = user?.email
+
+//        btn_edit.setOnClickListener {
+//            startActivity(Intent(view.context, SettingActivity::class.java))
+//        }
     }
 
 }
--- app/src/main/java/io/github/fuadreza/pikul_dagger/views/main/profile/ProfileState.kt
+++ app/src/main/java/io/github/fuadreza/pikul_dagger/views/main/profile/ProfileState.kt
@@ -1,7 +1,11 @@
 package io.github.fuadreza.pikul_dagger.views.main.profile
 
+import io.github.fuadreza.pikul_dagger.model.UserProfile
+
 sealed class ProfileState {
     object LoadProfileSuccess : ProfileState()
-    object LoadingState: ProfileState()
+    object LoadProfileError : ProfileState()
 
+    object LoadingState: ProfileState()
+    data class ProfileLoaded(var data: UserProfile) : ProfileState()
 }
